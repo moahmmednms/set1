83eff002662d673cadf2399d6bde193185860bdf
moahmmednms <41140381+moahmmednms@users.noreply.github.com>
2018-08-05 16:14:04 +0530

    Update even1.py

--- even1.py
+++ even1.py
@@ -1,6 +1,6 @@
-val1,val2=input().split()
-val1=int(val1)
-val2=int(val2)
-for i in range(val1+1,val2+1):
+a1,a2=input().split()
+a1=int(a1)
+a2=int(a2)
+for i in range(a1+1,a2+1):
     if(i % 2 == 0):
         print(i,end=" ")
